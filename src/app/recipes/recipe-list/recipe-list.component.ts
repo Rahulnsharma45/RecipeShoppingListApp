e386e6e8dedaa1ac00391ddb046b2fa60794bdaf
Rahul <rahul@pop-os.localdomain>
2020-01-27 04:16:32 +0530

    Displaying recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test','https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889913_960_720.jpg'),
+    new Recipe('A test recipe', 'This is simply a test','https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889913_960_720.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+new Recipe('A test recipe', 'This is simply a test','https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889913_960_720.jpg')
